ce24e91d386bc1500222ee50b2dae954c5da94a7
segunfrancis <francis4dblues@gmail.com>
2019-04-30 22:16:20 +0100

    Implement custom toast

--- app/src/main/java/com/example/computer/customtoast/MainActivity.java
+++ app/src/main/java/com/example/computer/customtoast/MainActivity.java
@@ -2,12 +2,36 @@ package com.example.computer.customtoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        container = findViewById(R.id.custom_toast_container);
+    }
+
+    public void displayToast(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, container);
+
+        TextView text = layout.findViewById(R.id.text);
+        text.setText("This is a custom toast");
+
+        Toast toast = new Toast(getApplicationContext());
+        toast.setGravity(Gravity.BOTTOM, 0, 40);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(layout);
+        toast.show();
     }
 }
