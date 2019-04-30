b69ab1a17e7289d77381c72e0508407626b3ba8e
segunfrancis <francis4dblues@gmail.com>
2019-04-30 22:41:47 +0100

    UI clean up

--- app/src/main/java/com/example/computer/customtoast/MainActivity.java
+++ app/src/main/java/com/example/computer/customtoast/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.computer.customtoast;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.CardView;
@@ -12,6 +13,8 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private CardView container;
+    private Context context;
+    private int duration = Toast.LENGTH_LONG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,19 +22,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         container = findViewById(R.id.custom_toast_container);
+        context = getApplicationContext();
     }
 
-    public void displayToast(View view) {
+    public void displayCustomToast(View view) {
         LayoutInflater inflater = getLayoutInflater();
         View layout = inflater.inflate(R.layout.custom_toast, container);
 
         TextView text = layout.findViewById(R.id.text);
-        text.setText("This is a custom toast");
+        text.setText(R.string.custom_toast_message);
 
-        Toast toast = new Toast(getApplicationContext());
+        Toast toast = new Toast(context);
         toast.setGravity(Gravity.BOTTOM, 0, 40);
-        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setDuration(duration);
         toast.setView(layout);
+        toast.show();
+    }
+
+    public void displayDefaultToast(View view) {
+        Toast toast = Toast.makeText(context, R.string.default_toast_message, duration);
+        toast.setGravity(Gravity.CENTER | Gravity.START, 20, 0);
         toast.show();
     }
 }
