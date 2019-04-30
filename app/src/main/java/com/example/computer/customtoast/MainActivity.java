a156492c53a991b94c912c2d9eeb7f69752c09be
segunfrancis <francis4dblues@gmail.com>
2019-04-30 21:24:14 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/computer/customtoast/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.computer.customtoast;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
